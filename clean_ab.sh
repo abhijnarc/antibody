8b9b93f2033e6a0ebf34c66e6b73af6c9d597c06
Abhijna R Chandra <92663128+abhijnarc@users.noreply.github.com>
2025-02-05 15:42:49 +0530

    Update clean_ab.sh

--- clean_ab.sh
+++ clean_ab.sh
@@ -3,10 +3,11 @@
 # get file prefix from command line
 pref=$1
 # set filenames
-input=${pref}.pdb
-output=${pref}_clean.pdb
+input=${pref}_igf.pdb
+output=${pref}_igf_clean.pdb
 hc=${pref}_H.pdb
 lc=${pref}_L.pdb
+
 # first the heavy chain
 cat $input | pdb_tidy -strict | pdb_selchain -H | pdb_delhetatm | \
          pdb_fixinsert | pdb_keepcoord | pdb_tidy -strict > $hc
